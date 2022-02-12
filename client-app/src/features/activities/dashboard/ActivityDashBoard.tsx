b3fa588d19ce95d29ebc499fa9c9ac488153ec9a
NTTE_gikyo_yujiyamashita <yuuji.yamashita.yg@east.ntt.co.jp>
2022-02-12 23:23:29 +0900

    66

--- client-app/src/features/activities/dashboard/ActivityDashBoard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashBoard.tsx
@@ -27,6 +27,7 @@ export default function ActivityDashBoard({activities, selectedActivity, deleteA
                 <ActivityList activities={activities}
                  selectActivity={selectActivity}
                  deleteActivity={deleteActivity}
+                 submitting={submitting}
                 />
             </Grid.Column>
             <Grid.Column width='6'>

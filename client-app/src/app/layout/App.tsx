920acf5660dd1d521987c2c0bd24a095408d8097
NTTE_gikyo_yujiyamashita <yuuji.yamashita.yg@east.ntt.co.jp>
2022-02-15 23:27:57 +0900

    76

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,9 +1,7 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { Container } from 'semantic-ui-react';
-import { Activity } from '../models/activity';
 import NavBar from './NavBar';
 import ActivityDashBoard from '../../features/activities/dashboard/ActivityDashBoard';
-import agent from '../api/agent';
 import LoadingComponent from './LoadingComponent';
 import { useStore } from '../stores/store';
 import { observer } from 'mobx-react-lite';
@@ -12,20 +10,9 @@ import { observer } from 'mobx-react-lite';
 function App() {
   const {activityStore} = useStore();
 
-  const [activities, setActivities] = useState<Activity[]>([]);
-  const [submitting, setSubmitting] = useState(false);
-
   useEffect(() => {
     activityStore.loadActivities();
   }, [activityStore])
-
-  function handleDeleteActivity(id: string) {
-    setSubmitting(true);
-    agent.Activities.delete(id).then(() => {
-      setActivities([...activities.filter(x => x.id !== id)]);
-      setSubmitting(false);
-    })
-  }
 
   if (activityStore.loadingInitial) return <LoadingComponent content='Loading app' />
 
@@ -34,11 +21,7 @@ function App() {
     <>
       <NavBar />
       <Container style={{marginTop: '7em'}}>
-        <ActivityDashBoard
-         activities={activityStore.activities}
-         deleteActivity={handleDeleteActivity}
-         submitting={submitting}
-        />
+        <ActivityDashBoard />
       </Container>
     </>
   );
--- client-app/src/features/activities/dashboard/ActivityList.tsx
+++ client-app/src/features/activities/dashboard/ActivityList.tsx
@@ -1,24 +1,18 @@
+import { observer } from 'mobx-react-lite';
 import React, { SyntheticEvent, useState } from 'react';
 import { Button, Item, ItemDescription, Label, Segment } from 'semantic-ui-react';
-import { Activity } from '../../../app/models/activity';
 import { useStore } from '../../../app/stores/store';
 
+export default observer (function ActivityList() {
+    const {activityStore} = useStore();
+    const {deleteActivity, activities, loading} = activityStore;
 
-interface Props {
-    activities: Activity[];
-    deleteActivity: (id: string) => void;
-    submitting: boolean;  
-}
-
-export default function ActivityList({activities, deleteActivity, submitting}: Props) {
     const [target, setTarget] = useState('');
 
     function handleActivityDelete(e: SyntheticEvent<HTMLButtonElement>, id: string) {
         setTarget(e.currentTarget.name);
         deleteActivity(id);
     }
-
-    const {activityStore} = useStore();
 
     return (
         <Segment>
@@ -36,7 +30,7 @@ export default function ActivityList({activities, deleteActivity, submitting}: P
                                 <Button onClick={() => activityStore.selectActivity(activity.id)} floated='right' content='View' color='blue' />
                                 <Button
                                     name={activity.id} 
-                                    loading={submitting && target === activity.id} 
+                                    loading={loading && target === activity.id} 
                                     onClick={(e) => handleActivityDelete(e, activity.id)} 
                                     floated='right' 
                                     content='Delete' 
@@ -50,4 +44,4 @@ export default function ActivityList({activities, deleteActivity, submitting}: P
             </Item.Group>
         </Segment>
     )
-}
+})

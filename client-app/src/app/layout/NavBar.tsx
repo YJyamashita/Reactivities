e4c6f54f014b893e49717cb843ff0591f9e8fc96
NTTE_gikyo_yujiyamashita <yuuji.yamashita.yg@east.ntt.co.jp>
2022-02-20 11:46:08 +0900

    84

--- client-app/src/app/layout/NavBar.tsx
+++ client-app/src/app/layout/NavBar.tsx
@@ -2,11 +2,8 @@ import React from 'react';
 import { observer } from 'mobx-react-lite';
 import { NavLink } from 'react-router-dom';
 import { Button, Container, Menu } from 'semantic-ui-react';
-import { useStore } from '../stores/store';
 
 export default observer(function NavBar() {
-
-    const {activityStore} = useStore();
     
     return (
         <Menu inverted fixed='top'>

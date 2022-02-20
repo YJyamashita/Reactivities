e8fe0bb775c414b863365b5f064411caf5e1ca90
NTTE_gikyo_yujiyamashita <yuuji.yamashita.yg@east.ntt.co.jp>
2022-02-20 11:19:31 +0900

    83

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -6,6 +6,7 @@ import { Route } from 'react-router-dom';
 import HomePage from '../../features/home/HomePage';
 import ActivityDashBoard from '../../features/activities/dashboard/ActivityDashBoard';
 import ActivityForm from '../../features/activities/form/ActivityForm';
+import ActivityDetails from '../../features/activities/details/ActivityDetails';
 
 
 function App() {
@@ -13,8 +14,9 @@ function App() {
     <>
       <NavBar />
       <Container style={{marginTop: '7em'}}>
-        <Route path='/' component={HomePage} />
-        <Route path='/activities' component={ActivityDashBoard} />
+        <Route exact path='/' component={HomePage} />
+        <Route exact path='/activities' component={ActivityDashBoard} />
+        <Route path='/activities/:id' component={ActivityDetails} />
         <Route path='/createActivity' component={ActivityForm} />
       </Container>
     </>

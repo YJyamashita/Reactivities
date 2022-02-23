9c6c15e4281b801fed839b5cd20754b44cfa2e53
NTTE_gikyo_yujiyamashita <yuuji.yamashita.yg@east.ntt.co.jp>
2022-02-23 20:11:29 +0900

    109

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -7,6 +7,8 @@ import HomePage from '../../features/home/HomePage';
 import ActivityDashBoard from '../../features/activities/dashboard/ActivityDashBoard';
 import ActivityForm from '../../features/activities/form/ActivityForm';
 import ActivityDetails from '../../features/activities/details/ActivityDetails';
+import TestErrors from '../../features/errors/TestError';
+import { ToastContainer } from 'react-toastify';
 
 
 function App() {
@@ -14,6 +16,7 @@ function App() {
 
   return (
     <>
+      <ToastContainer position='bottom-right' hideProgressBar />
       <Route exact path='/' component={HomePage} />
       <Route 
         path={'/(.+)'}
@@ -24,6 +27,7 @@ function App() {
               <Route exact path='/activities' component={ActivityDashBoard} />
               <Route path='/activities/:id' component={ActivityDetails} />
               <Route key={location.key} path={['/createActivity', '/manage/:id']} component={ActivityForm} />
+              <Route path='/errors' component={TestErrors} />
             </Container>
           </>
         )}

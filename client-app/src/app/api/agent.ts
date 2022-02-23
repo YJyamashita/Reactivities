bf8e202b922730e3da48086ac17580819ab761aa
NTTE_gikyo_yujiyamashita <yuuji.yamashita.yg@east.ntt.co.jp>
2022-02-23 22:08:57 +0900

    112

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosError, AxiosResponse } from 'axios';
 import { toast } from 'react-toastify';
 import { Activity } from '../models/activity';
-import { history } from '../../index';
+import { history } from '../..';
 
 const sleep = (delay: number) => {
     return new Promise((resolve) => {
@@ -15,10 +15,20 @@ axios.interceptors.response.use(async response => {
     await sleep(1000);
     return response;
 }, (error: AxiosError) => {
-    const {status} = error.response!;
+    const {data, status} = error.response!;
     switch (status) {
         case 400:
-            toast.error('bad request');
+            if (data.errors) {
+                const modalStateErrors = [];
+                for (const key in data.errors) {
+                    if (data.errors[key]) {
+                        modalStateErrors.push(data.errors[key])
+                    }
+                }
+                throw modalStateErrors.flat();
+            } else {
+                toast.error(data);
+            }
             break;
         case 401:
             toast.error('unauthorized');

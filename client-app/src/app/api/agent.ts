d5c79dac0f77d405a9741394a9894d24838188c2
NTTE_gikyo_yujiyamashita <yuuji.yamashita.yg@east.ntt.co.jp>
2022-02-25 23:26:59 +0900

    146

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -3,6 +3,7 @@ import { toast } from 'react-toastify';
 import { Activity } from '../models/activity';
 import { history } from '../..';
 import { store } from '../stores/store';
+import { User, UserFormValues } from '../../app/models/user';
 
 const sleep = (delay: number) => {
     return new Promise((resolve) => {
@@ -66,8 +67,15 @@ const Activities = {
     delete: (id: string) => axios.delete<void>(`/activities/${id}`)
 }
 
+const Account = {
+    current: () => requests.get<User>('/account'),
+    login: (user: UserFormValues) => requests.post<User>('/account/login', user),
+    register: (user: UserFormValues) => requests.post<User>('/account/register', user)
+}
+
 const agent = {
-    Activities
+    Activities,
+    Account
 }
 
 export default agent;

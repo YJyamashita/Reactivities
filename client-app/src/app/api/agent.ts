a4563e2341e273217482f0941e55fb25c169e536
NTTE_gikyo_yujiyamashita <yuuji.yamashita.yg@east.ntt.co.jp>
2022-02-28 23:34:30 +0900

    196

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -4,6 +4,7 @@ import { Activity, ActivityFormValues } from '../models/activity';
 import { history } from '../..';
 import { store } from '../stores/store';
 import { User, UserFormValues } from '../../app/models/user';
+import { Profile } from '../models/profile';
 
 const sleep = (delay: number) => {
     return new Promise((resolve) => {
@@ -80,9 +81,14 @@ const Account = {
     register: (user: UserFormValues) => requests.post<User>('/account/register', user)
 }
 
+const Profiles = {
+    get: (username: string) => requests.get<Profile>(`/profiles/${username}`)
+}
+
 const agent = {
     Activities,
-    Account
+    Account,
+    Profiles
 }
 
 export default agent;

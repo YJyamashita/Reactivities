630923050e8711b13e92c2ccc2aa3b889dea5910
NTTE_gikyo_yujiyamashita <yuuji.yamashita.yg@east.ntt.co.jp>
2022-03-03 23:28:25 +0900

    204

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -4,7 +4,7 @@ import { Activity, ActivityFormValues } from '../models/activity';
 import { history } from '../..';
 import { store } from '../stores/store';
 import { User, UserFormValues } from '../../app/models/user';
-import { Profile } from '../models/profile';
+import { Photo, Profile } from '../models/profile';
 
 const sleep = (delay: number) => {
     return new Promise((resolve) => {
@@ -82,7 +82,14 @@ const Account = {
 }
 
 const Profiles = {
-    get: (username: string) => requests.get<Profile>(`/profiles/${username}`)
+    get: (username: string) => requests.get<Profile>(`/profiles/${username}`),
+    uploadPhoto: (file: Blob) => {
+        let formData = new FormData();
+        formData.append('File', file);
+        return axios.post<Photo>('photos', formData, {
+            headers: {'Content-type': 'multipart/form-data'}
+        })
+    }
 }
 
 const agent = {

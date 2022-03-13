9943fd921eb8e19f80740f1a31a84888bf9ae374
NTTE_gikyo_yujiyamashita <yuuji.yamashita.yg@east.ntt.co.jp>
2022-03-13 22:15:38 +0900

    283

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -1,13 +1,17 @@
 using System.Linq;
 using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 using API.DTOs;
 using API.Services;
 using Domain;
+using Infrastructure.Email;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.WebUtilities;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace API.Controllers
 {
@@ -19,88 +23,100 @@ namespace API.Controllers
         private readonly UserManager<AppUser> _userManager;
         private readonly SignInManager<AppUser> _signInManager;
         private readonly TokenService _tokenService;
+        private readonly IConfiguration _config;
+        private readonly EmailSender _emailSender;
+
         public AccountController(UserManager<AppUser> userManager,
-        SignInManager<AppUser> signInManager, TokenService tokenService)
+            SignInManager<AppUser> signInManager, TokenService tokenService,
+            IConfiguration config, EmailSender emailSender)
         {
+            _config = config;
+            _emailSender = emailSender;
             _tokenService = tokenService;
             _signInManager = signInManager;
             _userManager = userManager;
         }
 
-        [HttpPost("login")]
-        public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
+    [HttpPost("login")]
+    public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
+    {
+        var user = await _userManager.Users.Include(p => p.Photos)
+            .FirstOrDefaultAsync(x => x.Email == loginDto.Email);
+
+        if (user == null) return Unauthorized("Invalid email");
+
+        if (user.UserName == "bob") user.EmailConfirmed = true;
+
+        if (!user.EmailConfirmed) return Unauthorized("Email not confirmed");
+
+        var result = await _signInManager.CheckPasswordSignInAsync(user, loginDto.Password, false);
+
+        if (result.Succeeded)
         {
-            var user = await _userManager.Users.Include(p => p.Photos)
-                .FirstOrDefaultAsync(x => x.Email == loginDto.Email);
-
-            if (user == null) return Unauthorized("Invalid email");
-
-            if (user.UserName == "bob") user.EmailConfirmed = true;
-
-            if (!user.EmailConfirmed) return Unauthorized("Email not confirmed");
-
-            var result = await _signInManager.CheckPasswordSignInAsync(user, loginDto.Password, false);
-
-            if (result.Succeeded)
-            {
-                return CreateUserObject(user);
-            }
-
-            return Unauthorized("Invalid password");
-        }
-
-        [HttpPost("register")]
-        public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
-        {
-            if (await _userManager.Users.AnyAsync(x => x.Email == registerDto.Email))
-            {
-                ModelState.AddModelError("email", "Email taken");
-                return ValidationProblem();
-            }
-            if (await _userManager.Users.AnyAsync(x => x.UserName == registerDto.Username))
-            {
-                ModelState.AddModelError("username", "Username taken");
-                return ValidationProblem();
-            }
-
-            var user = new AppUser
-            {
-                DisplayName = registerDto.DisplayName,
-                Email = registerDto.Email,
-                UserName = registerDto.Username
-            };
-
-            user.EmailConfirmed = true;
-
-            var result = await _userManager.CreateAsync(user, registerDto.Password);
-
-            if (result.Succeeded)
-            {
-                return CreateUserObject(user);
-            }
-
-            return BadRequest("Problem registering user");
-        }
-
-        [Authorize]
-        [HttpGet]
-        public async Task<ActionResult<UserDto>> GetCurrentUser()
-        {
-            var user = await _userManager.Users.Include(p => p.Photos)
-                .FirstOrDefaultAsync(x => x.Email == User.FindFirstValue(ClaimTypes.Email));
-
             return CreateUserObject(user);
         }
 
-        private UserDto CreateUserObject(AppUser user)
-        {
-            return new UserDto
-            {
-                DisplayName = user.DisplayName,
-                Image = user.Photos?.FirstOrDefault(x => x.IsMain)?.Url,
-                Token = _tokenService.CreateToken(user),
-                Username = user.UserName
-            };
-        }
+        return Unauthorized("Invalid password");
     }
+
+    [HttpPost("register")]
+    public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
+    {
+        if (await _userManager.Users.AnyAsync(x => x.Email == registerDto.Email))
+        {
+            ModelState.AddModelError("email", "Email taken");
+            return ValidationProblem();
+        }
+        if (await _userManager.Users.AnyAsync(x => x.UserName == registerDto.Username))
+        {
+            ModelState.AddModelError("username", "Username taken");
+            return ValidationProblem();
+        }
+
+        var user = new AppUser
+        {
+            DisplayName = registerDto.DisplayName,
+            Email = registerDto.Email,
+            UserName = registerDto.Username
+        };
+
+        user.EmailConfirmed = true;
+
+        var result = await _userManager.CreateAsync(user, registerDto.Password);
+
+        if (!result.Succeeded) return BadRequest("Problem registering user");
+
+        var origin = Request.Headers["origin"];
+        var token = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+        token = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(token));
+
+        var verifyUrl = $"{origin}/account/verifyEmail?token={token}&email={user.Email}";
+        var message = $"<p>Please click the below link to verify your email address:</p><p><a href='{verifyUrl}'>Click to verify email</a></p>";
+
+        await _emailSender.SendEmailAsync(user.Email, "Please verify email", message);
+
+        return Ok("Registration success - please verify email");
+    }
+
+    [Authorize]
+    [HttpGet]
+    public async Task<ActionResult<UserDto>> GetCurrentUser()
+    {
+        var user = await _userManager.Users.Include(p => p.Photos)
+            .FirstOrDefaultAsync(x => x.Email == User.FindFirstValue(ClaimTypes.Email));
+
+        return CreateUserObject(user);
+    }
+
+    private UserDto CreateUserObject(AppUser user)
+    {
+        return new UserDto
+        {
+            DisplayName = user.DisplayName,
+            Image = user.Photos?.FirstOrDefault(x => x.IsMain)?.Url,
+            Token = _tokenService.CreateToken(user),
+            Username = user.UserName
+        };
+    }
+}
 }
